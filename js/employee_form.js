7ec30b2ab973807458a71173eb7f58a6cd496a33
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-11-23 18:59:46 +0530

    Added code to update employee details

--- js/employee_form.js
+++ js/employee_form.js
@@ -1,3 +1,5 @@
+let isUpdate = false;
+let employeePayrollObj ={};
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
@@ -21,6 +23,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     salary.addEventListener('input', function() {
         output.textContent = salary.value;
     });
+    checkForUpdate();
 });
 
 const save = (event) => {
@@ -119,4 +122,44 @@ const setTextValue = (id, value) => {
 const setValue = (id, value) => {
     const element = document.querySelector(id);
     element.value = value;
+}
+
+const checkForUpdate = () =>{
+    const employeePayrollJson = localStorage.getItem('editEmp');
+    isUpdate = employeePayrollJson?true:false;
+    if(!isUpdate) return;
+    employeePayrollObj = JSON.parse(employeePayrollJson);
+    setForm();
+}
+
+const setForm = () => {
+    setValue('#name',employeePayrollObj._name);
+    setSelectedValues('[name=profile]',employeePayrollObj._profilePic);
+    setSelectedValues('[name=gender]',employeePayrollObj._gender);
+    setSelectedValues('[name=department]',employeePayrollObj._departments);
+    setValue('#salary',employeePayrollObj._salary);
+    setValue('#notes',employeePayrollObj._notes);
+    setTextValue('.salary-output',employeePayrollObj._salary);
+    let date= stringifyDate(employeePayrollObj._startDate).split(" ");
+    setSelectedIndex('#day',0);
+    setSelectedIndex('#month',0);
+    setSelectedIndex('#year',0);
+}
+
+const setSelectedValues = (propertyValue, value) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item =>{
+        if(Array.isArray(value)){
+            if(value.includes(item.value)){
+                item.checked = true;
+            }
+        }
+        else if (item.value===value)
+            item.checked = true;
+    })
+}
+
+const setSelectedIndex =(id,index) => {
+    const element =document.querySelector(id);
+    element.selectedIndex = index;
 }
